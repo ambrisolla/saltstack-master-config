fc86d01a0557ef327ba4fe41498263230dd064a0
ambrisolla <andremuzel@gmail.com>
2022-09-20 18:34:43 -0300

    add saltstack master configs

--- scripts/syncronize.sh
+++ scripts/syncronize.sh
@@ -11,16 +11,16 @@ SALT_CONFIG_DIR="/etc/salt"
 SALT_FILES_DIR="/srv"
 LOG_FILE="${WORKSPACE_DIR}/s/scripts/syncronize.log"
 
-sync_etc() {
-  echo -ne "\n\nStarting copy at: $(date)\n" >> ${LOG_FILE}
-  if rsync -av ${WORKSPACE_DIR}/s/etc/salt/* /etc/salt/ 2>> ${LOG_FILE} >> ${LOG_FILE}
-  then
-    echo -ne " - copying /etc/salt files... ok"
-  else
-    echo -ne " - copying /etc/salt files... fail"
-    cat ${LOG_FILE}
-    exit 1
-  fi
-}
-
-sync_etc
+#sync_etc() {
+#  echo -ne "\n\nStarting copy at: $(date)\n" >> ${LOG_FILE}
+#  if rsync -av ${WORKSPACE_DIR}/s/etc/salt/* /etc/salt/ 2>> ${LOG_FILE} >> ${LOG_FILE}
+#  then
+#    echo -ne " - copying /etc/salt files... ok"
+#  else
+#    echo -ne " - copying /etc/salt files... fail"
+#    cat ${LOG_FILE}
+#    exit 1
+#  fi
+#}
+#
+#sync_etc
