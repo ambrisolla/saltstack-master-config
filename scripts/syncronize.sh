fdb5aa78f4e75247194d60790b52bb5c34c709c2
ambrisolla <andremuzel@gmail.com>
2022-09-20 17:57:51 -0300

    add saltstack master configs

---
+++ scripts/syncronize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Author  : Andre Brisolla
+# Date    : Sep 20, 2022
+# Version : 0.1
+# Goal    : Syncronize Salt configuration files
+#
+
+
+
+sync_etc() {
+  echo "= $(Pipeline.Workspace) ="
+}
+
+sync_etc
