4495c04e8a56fcee078a91e4ef928d5cdf9b9464
ambrisolla <andremuzel@gmail.com>
2022-09-21 09:38:16 -0300

    add saltstack master configs

--- scripts/syncronize.sh
+++ scripts/syncronize.sh
@@ -12,35 +12,6 @@ SALT_FILES_DIR="/srv"
 LOG_FILE="${WORKSPACE_DIR}/s/scripts/syncronize.log"
 
 sync_etc() {
-  has_diff=0
-  for file in `find ${WORKSPACE_DIR}/s/etc -type f `; 
-  do 
-    dest_file=$(echo ${file} | sed -re "s|$WORKSPACE_DIR/s||g"  )
-    if ! diff $file $dest_file
-    then
-      if sudo rsync -a $file $dest_file
-      then
-        echo "Syncing \"${dest_file}\"... ok"
-      else
-        echo "Syncing \"${dest_file}\"... fail"
-      fi  
-      has_diff=1
-    fi
-  done
-  if [ $has_diff -eq 1 ]
-  then
-    if sudo systemctl restart salt-master
-    then
-      echo "Restarting Salt Master... ok"
-    else
-      echo "Restarting Salt Master... fail"
-      exit 1
-    fi
-  fi
-}
-
-
-sync_etc_2() {
   _rsync=$(sudo rsync -aEim ${WORKSPACE_DIR}/s/etc/salt/* /etc/salt/)
   if [ -n "$_rsync" ]
   then
@@ -59,4 +30,4 @@ sync_etc_2() {
 }
 
 #sync_etc
-sync_etc_2
+sync_etc
