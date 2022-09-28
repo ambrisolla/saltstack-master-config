b173d59d822ac1bda2adc399f549e25fb7ee532b
ambrisolla <andremuzel@gmail.com>
2022-09-28 19:05:40 -0300

    configure returner

--- srv/salt/states/returner/configure_db.sh
+++ srv/salt/states/returner/configure_db.sh
@@ -1,1 +1,8 @@
-echo "teste!"
+#!/bin/bash
+# Simple script to create a Salt Returner database into MySQL 
+
+CREATE_DATABASE="create database salt"
+CREATE_SCHEMA=$(cat /tmp/schema.sql)
+
+echo ${CREATE_DATABASE}
+echo ${CREATE_SCHEMA}
