2cac06a3cfd227c4332d6280224bab29847c0312
ambrisolla <andremuzel@gmail.com>
2022-09-29 14:46:57 -0300

    configuiring gpg

--- srv/salt/states/returner/configure_db.sh
+++ srv/salt/states/returner/configure_db.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Simple script to create a Salt Returner database into MySQL 
 
-CREATE_DATABASE="create database salt"
+
 CREATE_SCHEMA=$(cat /tmp/schema.sql)
 
-echo ${CREATE_DATABASE}
-echo ${CREATE_SCHEMA}
